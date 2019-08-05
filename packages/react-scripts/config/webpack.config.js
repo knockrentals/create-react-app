b2c52e23cf1133d1d9fbb9f97012a9940d455776
Gareth Small <gareth.dylan.small@gmail.com>
2019-08-05 14:03:35 -0700

    Added html-loader

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -533,6 +533,17 @@ module.exports = function(webpackEnv) {
                 'sass-loader'
               ),
             },
+            // Adds support for html files.
+            {
+              test: /\.(html)$/,
+              use: {
+                loader: 'html-loader',
+                options: {
+                  name: 'static/media/[name].[hash:8].[ext]',
+                  attrs: [':data-src']
+                }
+              }
+            },
             // "file" loader makes sure those assets get served by WebpackDevServer.
             // When you `import` an asset, you get its (virtual) filename.
             // In production, they would get copied to the `build` folder.
